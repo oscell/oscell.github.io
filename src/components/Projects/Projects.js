9e03b099e9efa85fbfd9e0484295840dc20fdd41
oscell <82882938+oscell@users.noreply.github.com>
2023-12-25 13:22:14 +0000

    Initial mockup

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Projects.css';
+import Image from '../../assets/image.jpg'
 
 function Projects() {
   return (
-    <div className='projects_container'>
-      <div className="categories">
-        <h2>My Projects</h2>
+    <div className='projects__container'>
+      <div className="categories__container">
+        <div className='projects__heading'>My Projects</div>
         <div className='project__categories'>
           <div className='project_category'>AI</div>
           <div className='project_category'>Web Dev</div>
@@ -15,7 +16,7 @@ function Projects() {
         </div>
       </div>
       <div className='projects'>
-        <div className='project_item'>
+        <div className='project_item' style={{ backgroundImage: `url(${Image})` }}>
           <div className='project__title'>
             Projects 1
           </div>
@@ -32,6 +33,38 @@ function Projects() {
           </div>
         </div>
         <div className='project_item'>
+          <div className='project__title'>
+            Projects 3
+          </div>
+          <div className='project__description'>
+            Description for Project 3
+          </div>
+        </div>
+        <div className='project_item'>
+          <div className='project__title'>
+            Projects 3
+          </div>
+          <div className='project__description'>
+            Description for Project 3
+          </div>
+        </div>
+        <div className='project_item'>
+          <div className='project__title'>
+            Projects 3
+          </div>
+          <div className='project__description'>
+            Description for Project 3
+          </div>
+        </div>
+        <div className='project_item'>
+          <div className='project__title'>
+            Projects 3
+          </div>
+          <div className='project__description'>
+            Description for Project 3
+          </div>
+        </div>
+                <div className='project_item'>
           <div className='project__title'>
             Projects 3
           </div>
